547648ebfd4dffb86c12677886d0cdbcfd546486
tolas4 <46448587+tolas4@users.noreply.github.com>
2019-01-09 15:14:12 +0800

    Add files via upload

---
+++ login_test.py
@@ -0,0 +1,8 @@
+from time import sleep
+import unittest, random, sys
+sys.path.append("./models")
+sys.path.append("./page_obj")
+from models import myunit, function
+
+
+class loginTest(myunit.MyTest):
--- loginpage.py
+++ loginpage.py
@@ -17,9 +17,9 @@ class LoginPage(Page):
     password_loc = (By.NAME, "p")
     submit_loc = (By.ID, 'login_button')
 
-    # Action
-    def to_iframe(self):
-        self.driver.switch_to(*self.iframe_loc)
+    #Action
+    # def to_iframe(self):
+    #     self.driver.switch_to.frame(*self.iframe_loc)
 
     def type_username(self, username):
         self.find_element(*self.username_loc).send_keys(username)
@@ -30,23 +30,26 @@ class LoginPage(Page):
     def submit(self):
         self.find_element(*self.submit_loc).click()
 
-    def test_user_login(self, driver=webdriver.Chrome()):
+    def test_user_login(self):
         """
         测试
         :param username:
         :param password:
         :return:
         """
+        driver = self.driver
         user_file = open('qquser.txt', 'r')
         lines = user_file.readline()
         user_file.close()
         username = lines.split(',')[0]
         password = lines.split(',')[1]
         print(username, password)
-        # driver.maximize_window()
-        # driver.implicitly_wait(10)
+        driver.maximize_window()
+
+        driver.implicitly_wait(10)
         self.open()
-        self.to_iframe()
+        # self.to_iframe()
+        driver.switch_to.frame("login_frame")
         self.type_username(username)
         self.type_password(password)
         self.submit()
